89168a2888ca9091c712f9ed70fe9b55798e5fa6
MohdBasurra <mohdye96@gmail.com>
2022-12-27 00:47:08 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser='salim';
+let welcomeMessage="Welcome to Flatbook, "+currentUser+"!";
+
+let excitedWelcomeMessage= "WELCOME TO FLATBOOK, "+currentUser+"!";
+ excitedWelcomeMessage= "WELCOME TO FLATBOOK, "+currentUser.toUpperCase()+"!";
+ let shortGreeting="Welcome, "+currentUser;
+ shortGreeting="Welcome, "+currentUser.slice(0,1)+"!";
+ console.log(shortGreeting);
